57717c044550bb09d3ae6b5f91bc7ff6cfe914f2
kerolos <kokoessa951@gmail.com>
2025-07-30 17:48:42 +0300

    feat: Update seller authentication component with login/signup toggle and styling

--- src/app/components/seller-auth/seller-auth.component.ts
+++ src/app/components/seller-auth/seller-auth.component.ts
@@ -2,14 +2,16 @@ import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { SellerService } from '../../services/seller.service';
 import { SellerModel } from '../../model/seller_model';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-seller-auth',
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './seller-auth.component.html',
   styleUrl: './seller-auth.component.css',
 })
 export class SellerAuthComponent {
+  showLogin = true;
   constructor(private sellerService: SellerService) {}
 
   ngOnInit() {
@@ -17,5 +19,16 @@ export class SellerAuthComponent {
   }
   signUp(signupForm: SellerModel): void {
     this.sellerService.userSignUp(signupForm);
+  }
+  Login(loginForm: any): void {
+    console.warn(loginForm);
+  }
+
+  openLogin() {
+    this.showLogin = true;
+  }
+
+  openSignUp() {
+    this.showLogin = false;
   }
 }
