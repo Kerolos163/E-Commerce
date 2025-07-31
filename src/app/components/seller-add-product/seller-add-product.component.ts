a52611149910513d82a10bcdd50ee3c6a5dd07cf
kerolos <kokoessa951@gmail.com>
2025-07-31 15:31:45 +0300

    feat: Implement seller add product form with styling and functionality

--- src/app/components/seller-add-product/seller-add-product.component.ts
+++ src/app/components/seller-add-product/seller-add-product.component.ts
@@ -1,11 +1,15 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-seller-add-product',
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './seller-add-product.component.html',
-  styleUrl: './seller-add-product.component.css'
+  styleUrl: './seller-add-product.component.css',
 })
 export class SellerAddProductComponent {
-
+  addProduct(productForm: any) {
+    console.warn(productForm);
+  }
 }
