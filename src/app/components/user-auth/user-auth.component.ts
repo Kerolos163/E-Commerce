02edc885bc356290077b6b47ecfe3520b115a1f7
kerolos <kokoessa951@gmail.com>
2025-08-03 21:01:06 +0300

    Implement local to remote cart synchronization in UserAuthComponent
    - Added `localCartToRemoteCart` method to synchronize local cart data with remote server upon user login.
    - Updated login logic to handle authentication errors and trigger cart synchronization.
    - Utilized `ProductService` to add cart items to the remote server.

--- src/app/components/user-auth/user-auth.component.ts
+++ src/app/components/user-auth/user-auth.component.ts
@@ -5,6 +5,8 @@ import { SellerUserModel } from '../../model/seller_model';
 import { SellerUserLoginModel } from '../../model/seller_login_model';
 import { UserService } from '../../services/user.service';
 import { Router } from '@angular/router';
+import { CartModel } from '../../model/cart_model';
+import { ProductService } from '../../services/product.service';
 
 @Component({
   selector: 'app-user-auth',
@@ -16,7 +18,10 @@ export class UserAuthComponent {
   showLogin = true;
   authError = '';
 
-  constructor(private userService: UserService) {}
+  constructor(
+    private userService: UserService,
+    private productService: ProductService
+  ) {}
 
   ngOnInit() {
     this.userService.userAuthReload();
@@ -37,9 +42,35 @@ export class UserAuthComponent {
   Login(loginForm: SellerUserLoginModel) {
     this.userService.userLogin(loginForm);
     this.userService.isLoginError.subscribe((isError) => {
+      console.warn(isError);
       if (isError) {
         this.authError = 'Email or Password is wrong';
+      } else {
+        this.localCartToRemoteCart();
       }
     });
+  }
+
+  localCartToRemoteCart() {
+    console.warn('localCartToRemoteCart');
+    let data = localStorage.getItem('localCart');
+    if (data) {
+      let cartDataList = JSON.parse(data);
+      let currentUser = localStorage.getItem('user');
+      let userId = currentUser && JSON.parse(currentUser).id;
+
+      cartDataList.forEach((item: any) => {
+        let cartItem: CartModel = {
+          userId: userId!,
+          product: item,
+        };
+        setTimeout(() => {
+          this.productService.addToCart(cartItem).subscribe((res) => {
+            console.warn(res);
+          });
+        }, 500);
+      });
+      localStorage.removeItem('localCart');
+    }
   }
 }
